45be9e880c17af876a311385046dd519ef7b0271
Amrinder <keptanamrinder@gmail.com>
2023-09-06 11:22:00 +0100

    fixed a bug regarding websocket

--- frontend/utils/socket.js
+++ frontend/utils/socket.js
@@ -1,5 +1,10 @@
 import { io } from "socket.io-client";
-const socket = io.connect("http://localhost:5000", {
+const URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://www.steinlux.com";
+const socket = io.connect(URL, {
   autoConnect: false,
 });
+console.log(process.env.NODE_ENV);
 export default socket;
